5a3ca064d10e945c38c74f8599699d7887c7b9d8
andre19fs <andres.pasten2005@gmail.com>
2025-08-22 21:01:10 -0400

    base de datos creada
    cuidado con errores que puedan surgir, comunicarlos

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./db/db');
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true}));
+
 
 app.listen(PORT, () => {
     console.log(`Server corriendo en http://localhost:${PORT}`);
