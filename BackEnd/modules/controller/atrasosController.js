60b5173a8d00d18db8b2ebf57b0ee342cdc2c170
andre19fs <andres.pasten2005@gmail.com>
2025-08-23 13:32:30 -0400

    atrasos

--- BackEnd/modules/controller/atrasosController.js
+++ BackEnd/modules/controller/atrasosController.js
@@ -1,2 +1,28 @@
 const loginModelo = require("../model/atrasosModel");
 
+exports.getAtrasos = (req, res) => {
+    const {fecha, hora_entrada, hora_salida} = req;
+
+    if (fecha, hora_entrada, hora_salida) {
+        return res.status(400).json({mensaje: "Faltan campos obligatorios"});
+    }
+
+    loginModelo.getAtrasos({fecha, hora_entrada, hora_salida}, (err, results) => {
+        if (err){
+            return res.status(500).json({
+                mensaje: 'Error interno del servidor',
+                error: err.message
+            });
+        }
+
+        if (results.length === 0) {
+            return req.status(404).json({
+                error: 'No hay registros con los parametros indicados'
+            });
+        }
+
+        res.json({
+            data: results
+        });
+    });
+}
--- BackEnd/modules/model/atrasosModel.js
+++ BackEnd/modules/model/atrasosModel.js
@@ -1,5 +1,21 @@
 const db = require('../../db/db');
 
 exports.getAtrasos = (data, callback) => {
-    { fecha_inicio, fecha_termino} = req;
+    const {fecha, hora_entrada, hora_salida} = data;
+
+    db.query('SELECT * FROM asistencias WHERE fecha = ? AND hora_entrada >= ? AND hora_salida <= ?', [fecha, hora_entrada, hora_salida], (err, results) => {
+        if (err) return callback(err);
+
+        if (!results || results.length === 0) return callback(new Error("Datos no disponibles"));
+
+        const reporte = results.map(r => ({
+            rut: r.rut,
+            fecha: r.fecha,
+            hora_entrada: r.hora_entrada,
+            hora_salida: r.hora_salida
+
+        }));
+
+        callback(null, {reporte});
+    });
 }
