9941c48c9ae5a319ab06cf95dbe2768eb9dd0b0d
andre19fs <andres.pasten2005@gmail.com>
2025-08-23 11:04:57 -0400

    correccion

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,30 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db/db');
-<<<<<<< HEAD
-const login = require('./modules/rutes/loginRutas')
-=======
-const trabajadorRutas = require('./modules/routes/trabajadorRutas');
 
->>>>>>> 5a31880ef7625d2f291a7abd7062ee73ba3436dc
+const login = require('./modules/rutes/loginRutas')
+const trabajadorRutas = require('./modules/routes/trabajadorRutas');
 
 const app = express();
 const PORT = 3000;
 
-<<<<<<< HEAD
 app.use(cors());  
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 
 app.use('/login', login);
-=======
-app.use(cors());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-
 app.use('/api/trabajadores', trabajadorRutas);
->>>>>>> 5a31880ef7625d2f291a7abd7062ee73ba3436dc
+
+
 
 app.listen(PORT, () => {
     console.log(`Server corriendo en http://localhost:${PORT}`);
