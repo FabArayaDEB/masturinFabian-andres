022734f4bace6b0e62f4d37b21bfd6bfdf86e8c7
andre19fs <andres.pasten2005@gmail.com>
2025-08-24 13:18:55 -0400

    ok

--- BackEnd/modules/routes/reportesRutas.js
+++ BackEnd/modules/routes/reportesRutas.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const atrasosController = require('../controller/atrasosController')
 const anticipadasController = require('../controller/anticiposController')
+const inasistenciasController = require('../controller/inasistenciasController')
 const {verificarToken, verificarRol} = require('../middleware')
 
 
-router.get('/reportes/atrasos', atrasosController.getAtrasos);
+router.get('/reportes/atrasos', verificarToken, verificarRol('admin'), atrasosController.getAtrasos);
 router.get('/reportes/anticipos', verificarToken, verificarRol('admin'), anticipadasController.getAnticipadas);
+router.get('/inasistencias', verificarToken, verificarRol('admin'), inasistenciasController.getInasistencias);
 
 module.exports = router;
